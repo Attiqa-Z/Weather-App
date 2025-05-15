6c0991a122eeb16b5814f7ec6a7beab1c6bb270c
Attiqa-Z <attiqazaki1@gmail.com>
2025-05-15 11:36:43 +0500

    Complete it

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,80 @@
-import "./index.css"
-function App() {
-  return (
-    <div className="p-4">
-      <p className="text-base text-gray-800">
-        This is the simplest project for understanding React Router DOM.
-      </p>
-      <div>
-      <button className="bg-sky-800 hover:bg-sky-300 ...">Save changes</button>
-      </div>
-      <div className="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
-  <div>
-    <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
-      <svg
-        className="h-6 w-6 text-white"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        aria-hidden="true"
-      >
-      </svg>
-    </span>
-  </div>
-  <h3 className="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
-  <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
-    The Zero Gravity Pen can be used to write in any orientation. It even works in outer space.
-  </p>
-</div>
-<img className="rotate-x-50 rotate-z-45..." src="/src/assets/love.jpg" />
+import React, { useState } from "react";
+import axios from "axios";
 
-   </div>
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+  };
+  weather: {
+    main: string;
+  }[];
+  wind: {
+    speed: number;
+  };
+}
+
+function App() {
+  const [data, setData] = useState<WeatherData | null>(null);
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+
+  const searchLocation = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDown={searchLocation} 
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data?.name}</p>
+          </div>
+          <div className="temp">
+            {data?.main && <h1>{data.main.temp.toFixed()}°F</h1>}
+          </div>
+          <div className="description">
+            {data?.weather && <p>{data.weather[0].main}</p>}
+          </div>
+        </div>
+
+        {data && (
+          <div className="bottom">
+            <div className="feels">
+              <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              <p className="bold">{data.main.humidity}%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
 export default App;
-
